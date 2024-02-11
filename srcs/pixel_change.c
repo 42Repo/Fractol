2a7a90f40df3c0c18347b6245cde8d2f4f7521d9
asuc <sucaurel@gmail.com>
2024-02-11 10:35:31 +0100

    fix norm

--- srcs/pixel_change.c
+++ srcs/pixel_change.c
@@ -6,7 +6,7 @@
 /*   By: asuc <asuc@student.42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 17:55:20 by asuc              #+#    #+#             */
-/*   Updated: 2024/02/05 18:24:46 by asuc             ###   ########.fr       */
+/*   Updated: 2024/02/11 10:29:40 by asuc             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	add_pixel_size(t_data *data)
 {
-	if (data->pixel_size < 16)
+	if (data->pixel_size < 12)
 		data->pixel_size++;
 }
 
@@ -43,7 +43,8 @@ void	put_pixel_art(t_data *data, int x, int y, unsigned int color)
 			if (x + dx < WIDTH && y + dy < HEIGHT)
 			{
 				color += 0xFF000000;
-				mlx_set_image_pixel(data->mlx, data->img, x + dx, y + dy, color);
+				mlx_set_image_pixel(data->mlx, data->img,
+					x + dx, y + dy, color);
 			}
 			dy++;
 		}
